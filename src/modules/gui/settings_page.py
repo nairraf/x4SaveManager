147e8df6768f101ce9eb3a5dabb6780a14af0732
Ian Farr <ian.farr@farrworks.com>
2023-02-05 16:02:28 -0500

    getting ready for the settings work

--- src/modules/gui/settings_page.py
+++ src/modules/gui/settings_page.py
@@ -1,12 +1,13 @@
 import tkinter as tk
 from tkinter import ttk
 from .gui_settings import GuiSettings
-from .messages import MessageWindow
 
 class Settings(tk.Toplevel):
     def __init__(self, caller):
         super().__init__()
         self.withdraw()
+        self.status_text = tk.StringVar()
+        self.app_config_path_text = tk.StringVar()
         self.caller = caller
         self.rowconfigure(0, weight=1)
         self.columnconfigure(0, weight=1)
@@ -20,9 +21,35 @@ class Settings(tk.Toplevel):
         )
         self.minsize(450,300)
         
-        nb = ttk.Notebook(self, width=450, height=300)
-        app_page = ttk.Frame(nb)
-        db_page = ttk.Frame(nb)
+        nb = ttk.Notebook(
+            self,
+            width=450,
+            height=300,
+            padding=5
+        )
+        # app page
+        app_page = ttk.Frame(nb, padding=5)
+        app_page.grid_columnconfigure(1, weight=1)
+        ttk.Label(app_page, text='Application Config Path:').grid(
+            column=0,
+            row=0,
+            sticky=tk.W
+        )
+        self.app_config_path = ttk.Entry(
+            app_page,
+            textvariable=self.app_config_path_text,
+            width=30
+        )
+        self.app_config_path.grid(
+            column=1,
+            row=0,
+            sticky=(tk.E, tk.W)
+        )
+        # database page
+        db_page = ttk.Frame(nb, padding=5)
+        db_page.grid_columnconfigure(1, weight=1)
+
+        # add the pages to our notebook
         nb.add(app_page, text="App Settings")
         nb.add(db_page, text="Database Settings")
         nb.grid(
@@ -33,7 +60,24 @@ class Settings(tk.Toplevel):
             sticky=(tk.N, tk.E, tk.S, tk.W)
         )
 
-        ttk.Button(self, text="Save", command=self.save).grid(
+        # status bar and save/close buttons
+        self.status = ttk.Label(
+            self,
+            anchor=tk.E,
+            textvariable=self.status_text
+        )
+        self.status.grid(
+            column=0,
+            row=1,
+            sticky=(tk.E, tk.W)
+        )
+        self.save = ttk.Button(
+            self,
+            text="Save",
+            state="disabled",
+            command=self.save
+        )
+        self.save.grid(
             column=1,
             row=1,
             sticky=tk.E
@@ -45,6 +89,7 @@ class Settings(tk.Toplevel):
             sticky=tk.E
         )
 
+        # place and show the status page
         self.update()
         self.geometry("+{}+{}".format(
             self.master.winfo_x() + 50,
@@ -53,16 +98,11 @@ class Settings(tk.Toplevel):
         self.transient(self.master)
         self.deiconify()
         self.focus()
-        #self.wait_visibility()
-        #self.grab_set()
-        #self.wait_window()
 
     def close(self):
         self.destroy()
 
     def save(self):
-        MessageWindow(
-            self,
-            "Saved Settings"
-        )
+        self.status_text.set("Settings Saved")
+        self.save.state(['disabled'])
 
